92d2c89c81184fa97c3aaa9de9de52756b428ffb
codesparshofficial <codesparsh.official@gmail.com>
2025-03-01 23:22:25 -0800

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -110,7 +110,7 @@ document.getElementById("placeOrderBtn").addEventListener("click", function() {
         email: userEmail,
         contact: orderDetails.mobile
     },
-    theme: { color: "#d40000" },
+    theme: { color: "linear-gradient(135deg, #FFB3B3, #FFD1C1)" },
     modal: { ondismiss: function() { alert("⚠ Payment was cancelled. Please try again."); } }
   };
 
